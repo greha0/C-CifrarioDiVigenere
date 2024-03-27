2d9c2119f799f356bfbed2b5ab93231179188b5b
greta <greta.maria.brugnatti.2007@calvino.edu.it>
2024-03-27 08:55:29 +0100

    fgets

--- header.h
+++ header.h
@@ -11,4 +11,9 @@
 #ifndef C_CIFRARIODIVIGENERE_HEADER_H
 #define C_CIFRARIODIVIGENERE_HEADER_H
 
+char toLower(char c);
+void eliminaACapo(char str[]);
+void codificaIlMessaggio(char strCod[], char strDec[]);
+void decifraIlMessaggio(char strCod[], char strDec[]);
+
 #endif //C_CIFRARIODIVIGENERE_HEADER_H
--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "header.h"
+
+#define DIM 100
 
 /*
  *
@@ -14,6 +17,10 @@
 
 int main (){
     int scelta=1;
+    char messaggioCod[DIM];
+    char messaggioDec[DIM];
+    char verme[]="gatto";
+    messaggioDec[0]= 'a';
 
     do{
         printf("Case: ");
@@ -23,12 +30,49 @@ int main (){
 
             case 1: //CODIFICA
 
+                fflush(stdin);
+                printf("Inserire il codice da codificare: ");
+                fgets(messaggioDec, DIM, stdin);
+                eliminaACapo(messaggioDec);
+
+
+                fflush(stdin);
+                printf("Inserire il verme: ");
+                fgets(verme, 100, stdin);
+                eliminaACapo(verme);
+
+                printf("%s\n", messaggioDec);
+
                 break;
+
+
+
 
             case 2: //DECODIFICA
 
+                fflush(stdin);
+                fgets(messaggioCod, 500, stdin);
+                eliminaACapo(messaggioCod);
                 break;
         }
 
     } while (scelta!=0);
+}
+
+char toLower(char c){
+    if(c >= 'A' && c <= 'Z'){
+        c = c -'A' + 'a';
+    }
+    return c;
+}
+
+void eliminaACapo(char str[]){
+
+    int i;
+
+    for(i=0; str[i]!='\n'; i++){
+
+    }
+
+    str[i]='\0';
 }
